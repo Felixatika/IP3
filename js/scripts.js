a7c9ea146131a2c3b6b7b5fc5c3e30dbdd0df839
Felixatika <felixatika@gmail.com>
2020-08-14 08:35:16 +0300

    add project directories and some content

---
+++ js/scripts.js
@@ -0,0 +1,123 @@
+$(document).ready(function () {
+  $(".content").hide();
+
+  $("#design").click(function () {
+    $(".content").slideToggle();
+  });
+  $(".development").hide();
+  $("#dev").click(function () {
+    $(".development").slideToggle();
+  });
+  $(".product-sample").hide();
+  $("#product").click(function () {
+    $(".product-sample").slideToggle();
+  });
+});
+$(document).ready(function () {
+  $("#work1")
+    .mouseover(function () {
+      $("#overlay").show();
+      var overlay = document.getElementById("overlay");
+      overlay.style.border = "3px white solid";
+      overlay.style.width = "82%";
+      overlay.style.height = "80%";
+    })
+    .mouseout(function () {
+      $("#overlay").hide();
+    });
+});
+$(document).ready(function () {
+  $("#work2")
+    .mouseover(function () {
+      $("#overlay1").show();
+      var overlay = document.getElementById("overlay1");
+      overlay.style.border = "3px white solid";
+      overlay.style.width = "82%";
+      overlay.style.height = "80%";
+    })
+    .mouseout(function () {
+      $("#overlay1").hide();
+    });
+});
+$(document).ready(function () {
+  $("#work3")
+    .mouseover(function () {
+      $("#overlay2").show();
+      var overlay = document.getElementById("overlay2");
+      overlay.style.border = "3px white solid";
+      overlay.style.width = "82%";
+      overlay.style.height = "80%";
+    })
+    .mouseout(function () {
+      $("#overlay2").hide();
+    });
+});
+
+$(document).ready(function () {
+  $("#work4")
+    .mouseover(function () {
+      $("#overlay3").show();
+      var overlay = document.getElementById("overlay3");
+      overlay.style.border = "3px white solid";
+      overlay.style.width = "82%";
+      overlay.style.height = "80%";
+    })
+    .mouseout(function () {
+      $("#overlay3").hide();
+    });
+});
+
+$(document).ready(function () {
+  $("#work5")
+    .mouseover(function () {
+      $("#overlay4").show();
+      var overlay = document.getElementById("overlay4");
+      overlay.style.border = "3px white solid";
+      overlay.style.width = "82%";
+      overlay.style.height = "80%";
+    })
+    .mouseout(function () {
+      $("#overlay4").hide();
+    });
+});
+
+$(document).ready(function () {
+  $("#work6")
+    .mouseover(function () {
+      $("#overlay5").show();
+      var overlay = document.getElementById("overlay5");
+      overlay.style.border = "3px white solid";
+      overlay.style.width = "82%";
+      overlay.style.height = "80%";
+    })
+    .mouseout(function () {
+      $("#overlay5").hide();
+    });
+});
+
+$(document).ready(function () {
+  $("#work7")
+    .mouseover(function () {
+      $("#overlay6").show();
+      var overlay = document.getElementById("overlay6");
+      overlay.style.border = "3px white solid";
+      overlay.style.width = "82%";
+      overlay.style.height = "80%";
+    })
+    .mouseout(function () {
+      $("#overlay6").hide();
+    });
+});
+$(document).ready(function () {
+  $("#work8")
+    .mouseover(function () {
+      $("#overlay7").show();
+      var overlay = document.getElementById("overlay7");
+      overlay.style.border = "3px white solid";
+      overlay.style.width = "82%";
+      overlay.style.height = "80%";
+    })
+    .mouseout(function () {
+      $("#overlay7").hide();
+    });
+});
